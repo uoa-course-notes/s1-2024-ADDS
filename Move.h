8eb10fc61dadd42a73ea4c844549402ef543d185
Tang Piseth <tangpiseth43@gmail.com>
2024-03-07 13:58:19 +1030

    Added folders for testing

---
+++ Move.h
@@ -0,0 +1,23 @@
+#ifndef MOVE_H
+#define MOVE_H
+#include <string>
+
+
+class Move{
+	private:
+	    std::string name ;
+	    std::string relationship;
+	public:
+	    //Move();
+	    
+	    virtual ~Move() = 0;
+
+
+
+};
+
+
+
+
+
+#endif
