f7ebfc8f2d7712ad47f536960722d13efa2e874f
Tang Piseth <tangpiseth43@gmail.com>
2024-03-27 15:11:59 +1030

    Test submission with GS

---
+++ Finder.cpp
@@ -0,0 +1,18 @@
+#include "Finder.h"
+
+
+
+std::vector<int> Finder::findSubstrings(std::string s1, std::string s2){
+    std::vector<int> result;
+    for (size_t i = 1; i <= s2.size(); i++){
+        size_t found = s1.find(s2.substr(0, i));
+        if (found != std::string::npos) result.push_back(found);
+        else result.push_back(-1);
+    }
+
+    return result;
+
+}
+
+
+
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "Finder.h"
+
+
+
+int main(int argc, char** argv){
+    Finder finder;
+    std::string s1 = "4634554567";
+    std::string s2 = "45689";
+    // finder.findSubstrings(s1, s2);
+    std::cout << std::string::npos << std::endl;
+
+    // std::cout << s1.find("5") << std::endl;
+    
+
+
+
+    return 0;
+}
