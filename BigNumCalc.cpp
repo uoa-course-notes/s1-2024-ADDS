a8adb0b9d3b81a53bdc8d9a69bba7166a22a8b30
Tang Piseth <tangpiseth43@gmail.com>
2024-04-28 18:44:08 +0930

    I would like to know who created the assignments. You should do a better job. Else, learn more.

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -9,7 +9,29 @@ BigNumCalc::BigNumCalc(int x){
 
 }
 
+std::list<int> BigNumCalc::buildBigNum(std::string numString){
+    return {};
+}
 
+// Add num1 and num2
+std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2){
+    return {};
+
+}
+
+// Subtract num2 from num1
+std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2){
+    return {};
+
+}
+    
+// Multiply num1 and num2
+std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2){
+    return {};
+
+}
+// For simplicity, assume that num2 will be only one digit long. 
+        
 
 
 BigNumCalc::~BigNumCalc(){
