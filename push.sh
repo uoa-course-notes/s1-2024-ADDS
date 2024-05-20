a9b0b8b59539a4d4fbd8b2d53888e37e7d2d71e9
Tang Piseth <tangpiseth43@gmail.com>
2024-05-20 17:57:54 +0930

    Trying out new bash file

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+
+# Check if a commit message is provided 
+if [ -z "$1"]; then 
+	echo "Please provide a commit message"
+	exit 1 
+fi 
+
+
+# Add all changes 
+git add .
+
+
+# Commit the changes with the provided message 
+git commit -m "$1"
+
+
+
+# Push to the remote branch 
+git push origin practical_11
